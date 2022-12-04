86ee715b0520bcb18cd9af444694666022bebcef
Ivan Molto <ivanmolto@gmail.com>
2022-12-04 10:48:51 +0100

    chore: Add Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRoutes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Market from "./components/Market";
+import Wallets from "./components/Wallets";
+import WalletProfiler from "./components/WalletProfiler";
+import Wallet from "./components/Wallet";
+import Collections from "./components/Collections";
+import CollectionProfiler from "./components/CollectionProfiler";
+import Collection from "./components/Collection";
+import NftProfiler from "./components/NftProfiler";
+import Nft from "./components/Nft";
+import NoMatch from "./components/NoMatch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Routes() {
+  return useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "market", element: <Market /> },
+        { path: "walletprofiler", element: <WalletProfiler /> },
+        {
+          path: "wallets",
+          element: <Wallets />,
+          children: [
+            { path: "", element: <WalletProfiler /> },
+            { path: ":addressId", element: <Wallet /> },
+          ],
+        },
+        { path: "collectionprofiler", element: <CollectionProfiler /> },
+        { path: "nftprofiler", element: <NftProfiler /> },
+        {
+          path: "collections",
+          element: <Collections />,
+          children: [
+            { path: "", element: <CollectionProfiler /> },
+            { path: ":collectionId", element: <Collection /> },
+          ],
+        },
+        { path: "nfts", element: <NftProfiler /> },
+        {
+          path: "nfts/:collectionId/:tokenId",
+          element: <Nft />,
+        },
+        { path: "*", element: <NoMatch /> },
+      ],
+    },
+  ]);
 }
+
+const App = () => {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+};
 
 export default App;

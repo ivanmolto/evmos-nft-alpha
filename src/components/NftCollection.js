7cb70636166d20ce6ef81553a8847f34a17d189e
Ivan Molto <ivanmolto@gmail.com>
2022-12-04 14:20:45 +0100

    chore: Add Error boundaries

---
+++ src/components/NftCollection.js
@@ -0,0 +1,272 @@
+import { useQuery } from "@tanstack/react-query";
+import { ArrowCircleRightIcon } from "@heroicons/react/outline";
+
+import { Link } from "react-router-dom";
+import NftLogo from "./NftLogo";
+import NftDescription from "./NftDescription";
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getCollection(collection) {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+  return fetch(
+    `https://api.covalenthq.com/v1/9001/tokens/${collection}/nft_token_ids/?&key=${process.env.REACT_APP_COVALENT_SECRET_KEY}`,
+    options
+  ).then((response) => response.json());
+}
+
+const NftCollection = ({ collection }) => {
+  const collectionQuery = useQuery({
+    queryKey: ["collection", collection],
+    queryFn: () => getCollection(collection),
+  });
+
+  const data = collectionQuery.data;
+  if (collectionQuery.isLoading)
+    return (
+      <div className="min-h-full font-montserrat">
+        <main className="flex-1 pb-8">
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <h2 className="font-bold text-xl">NFT Collection</h2>
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-eorange"></div>
+            Loading collection...
+          </div>
+        </main>
+      </div>
+    );
+
+  if (collectionQuery.isError)
+    return <p>Error: {collectionQuery.error.message}</p>;
+  if (data === null)
+    return (
+      <div className="min-h-full font-montserrat">
+        <main className="flex-1 pb-8">
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <h2 className="font-bold text-xl">NFT Collection</h2>
+            <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-eorange"></div>
+            Sorry, no data available for this contract address
+          </div>
+        </main>
+      </div>
+    );
+  if (collection === "0xabbaa322a763b36587e3f63e46a81deacb2957a7")
+    return (
+      <div className="bg-gray-50 font-montserrat">
+        <div className="mt-1">
+          <h2 className="max-w-6xl mx-auto mt-0 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
+            NFT Collection
+          </h2>
+          <div>
+            <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+              <div className="py-3 md:flex md:items-center md:justify-between lg:border-t border-eorange lg:border-eorange"></div>
+            </div>
+          </div>
+          <div className="mx-auto max-w-2xl py-2 px-4 sm:py-0 sm:px-6 lg:grid lg:max-w-6xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+            <div className="lg:max-w-lg lg:self-end">
+              <div className="mt-4">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                  Evmos Domains
+                </h1>
+              </div>
+              <section aria-labelledby="information-heading" className="mt-4">
+                <h2 id="information-heading" className="sr-only">
+                  Collection information
+                </h2>
+
+                <div className="flex items-center">
+                  <p className="text-lg text-gray-900 sm:text-xl">
+                    Items:{" "}
+                    <span className="font-semibold">
+                      {numberWithCommas(data.data.items.length)}
+                    </span>
+                    <br />
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <p className="text-lg text-gray-900 sm:text-xl">
+                    Symbol: <span className="font-semibold">ED</span>
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <p className="text-lg text-gray-900 sm:text-xl">
+                    Contract Address:{" "}
+                    <span className="font-semibold">
+                      {collection.slice(0, 6)}...{collection.slice(-4)}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <p className="text-lg text-gray-900 sm:text-xl">
+                    Chain: <span className="font-semibold">Evmos</span>
+                  </p>
+                </div>
+                <NftDescription collection={collection} />
+                <div></div>
+                <div className="mt-6 space-y-6">
+                  <a
+                    href={`https://evm.evmos.org/address/${collection}`}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                  >
+                    View on Evmos EVM Explorer{" "}
+                    <ArrowCircleRightIcon
+                      className="h-4 w-4"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </div>
+
+                <div className="mt-2 flex items-center">
+                  <a
+                    href={`https://www.orbitmarket.io/collection/${collection}`}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                  >
+                    View on Orbit Market
+                    <ArrowCircleRightIcon
+                      className="h-4 w-4"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </div>
+
+                <div className="mt-2 flex items-center">
+                  <Link
+                    to="/nftprofiler"
+                    className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                  >
+                    NFT Item Profiler
+                    <ArrowCircleRightIcon
+                      className="h-4 w-4"
+                      aria-hidden="true"
+                    />
+                  </Link>
+                </div>
+              </section>
+            </div>
+            <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+              <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+                <NftLogo collection={collection} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  return (
+    <div className="bg-gray-50 font-montserrat">
+      <div className="mt-1">
+        <h2 className="max-w-6xl mx-auto mt-0 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
+          NFT Collection
+        </h2>
+        <div>
+          <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div className="py-3 md:flex md:items-center md:justify-between lg:border-t border-eorange lg:border-eorange"></div>
+          </div>
+        </div>
+        <div className="mx-auto max-w-2xl py-2 px-4 sm:py-0 sm:px-6 lg:grid lg:max-w-6xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:max-w-lg lg:self-end">
+            <div className="mt-4">
+              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                {data.data.items[0].contract_name || "Collection name n/a"}
+              </h1>
+            </div>
+
+            <section aria-labelledby="information-heading" className="mt-4">
+              <h2 id="information-heading" className="sr-only">
+                Collection information
+              </h2>
+
+              <div className="flex items-center">
+                <p className="text-lg text-gray-900 sm:text-xl">
+                  Items:{" "}
+                  <span className="font-semibold">
+                    {numberWithCommas(data.data.items.length)}
+                  </span>
+                  <br />
+                </p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-lg text-gray-900 sm:text-xl">
+                  Symbol:{" "}
+                  <span className="font-semibold">
+                    {data.data.items[0].contract_ticker_symbol || "n/a"}
+                  </span>
+                </p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-lg text-gray-900 sm:text-xl">
+                  Contract Address:{" "}
+                  <span className="font-semibold">
+                    {collection.slice(0, 6)}...{collection.slice(-4)}
+                  </span>
+                </p>
+              </div>
+              <div className="flex items-center">
+                <p className="text-lg text-gray-900 sm:text-xl">
+                  Chain: <span className="font-semibold">Evmos</span>
+                </p>
+              </div>
+              <NftDescription collection={collection} />
+              <div></div>
+
+              <div className="mt-6 space-y-6">
+                <a
+                  href="https://evm.evmos.org/address/0x4C275aDE386Af17276834579b1A68146cef3C770"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                >
+                  View on Evmos EVM Explorer{" "}
+                  <ArrowCircleRightIcon
+                    className="h-4 w-4"
+                    aria-hidden="true"
+                  />
+                </a>
+              </div>
+
+              <div className="mt-2 flex items-center">
+                <a
+                  href="https://www.orbitmarket.io/collection/0x4c275ade386af17276834579b1a68146cef3c770"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                >
+                  View on Orbit Market
+                  <ArrowCircleRightIcon
+                    className="h-4 w-4"
+                    aria-hidden="true"
+                  />
+                </a>
+              </div>
+
+              <div className="mt-2 flex items-center">
+                <Link
+                  to="/nftprofiler"
+                  className="text-base text-eorange hover:text-eviolet inline-flex items-center"
+                >
+                  NFT Item Profiler
+                  <ArrowCircleRightIcon
+                    className="h-4 w-4"
+                    aria-hidden="true"
+                  />
+                </Link>
+              </div>
+            </section>
+          </div>
+          <div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+            <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg">
+              <NftLogo collection={collection} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NftCollection;
---
+++ src/components/NftLogo.js
@@ -0,0 +1,135 @@
+import { useQuery } from "@tanstack/react-query";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+const fallbackImage = {
+  na: "https://ivanmolto.mypinata.cloud/ipfs/QmSYRfysyk5n5YqMN6Zw2fCZdvZsJvhETmhgQKTdAfPbwb", // "https://ivanmolto.mypinata.cloud/ipfs/QmdqYqXFr2UsiDG454qESUsTfmAtm1hqmteSfiWhVXoGKL",
+  bored:
+    "https://ivanmolto.mypinata.cloud/ipfs/QmQdgiy2EqCSJNv1PcxXNBuLZjgUz4i35M94yfEbL13m2T",
+  pdao: "https://ivanmolto.mypinata.cloud/ipfs/Qmchyp2Fc6KKS62CUtssqyShjYSdGB1AU5X8eCV7Gpfwdu",
+  toadz:
+    "https://ivanmolto.mypinata.cloud/ipfs/QmbGYjLW5Kwr7Bm2QuWuVVTCRfcSZEZHiQhtHA6uS4Yvcz",
+  domains:
+    "https://ivanmolto.mypinata.cloud/ipfs/QmaHo1Bw2KCVvGUKisPTPDCnaiB8awjkiAac8uk6DPBKio",
+};
+
+function getFileExtension(filename) {
+  return filename.split(".").pop();
+}
+
+function getLogo(collection) {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+  return fetch(
+    `https://api.covalenthq.com/v1/9001/tokens/${collection}/nft_metadata/1/?&key=${process.env.REACT_APP_COVALENT_SECRET_KEY}`,
+    options
+  ).then((response) => response.json());
+}
+
+const NftLogo = ({ collection }) => {
+  const logoQuery = useQuery({
+    queryKey: ["logo", collection],
+    queryFn: () => getLogo(collection),
+  });
+
+  const data = logoQuery.data;
+  if (logoQuery.isLoading) return <div>Loading image...</div>;
+  if (logoQuery.isError) return <div>Error: {logoQuery.error.message}</div>;
+  if (data === null)
+    return (
+      <LazyLoadImage
+        src={`https://ivanmolto.mypinata.cloud/ipfs/QmbLmd6Zy7AU5oS9Ww6PyHZamV6QT5rF3yEWTSCvkLxLTk`}
+        alt="No image available"
+        className="rounded-md"
+      />
+    );
+  if (collection === "0xabbaa322a763b36587e3f63e46a81deacb2957a7")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.domains}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+  if (collection === "0x6ad439c980d6eee165a256d1026cc98d9739d6bf")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.na}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+  if (collection === "0x0ec23669dad631a159f66c865beb60266b16d157")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.na}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+  if (collection === "0x31782794fd38803e16bc4d7c504cf31e8e6dd282")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.bored}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+  if (collection === "0x348118c75b91a8a95d47d3a0da7c2d619a8deed4")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.toadz}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+  if (collection === "0xc1d5224bd38e79042037ea75f908a22275669691")
+    return (
+      <div className="h-full w-full object-cover object-center">
+        <LazyLoadImage
+          src={fallbackImage.pdao}
+          alt="Collection First NFT"
+          className="rounded-md"
+        />
+      </div>
+    );
+
+  if (logoQuery.isSuccess)
+    return (
+      <div>
+        {getFileExtension(
+          data.data.items[0].nft_data[0].external_data.image
+        ) === "mp4" && (
+          <video
+            autoPlay
+            src={data.data.items[0].nft_data[0].external_data.image}
+            type="video/mp4"
+          ></video>
+        )}
+
+        {collection !== "0xc1d5224bd38e79042037ea75f908a22275669691" &&
+          getFileExtension(
+            data.data.items[0].nft_data[0].external_data.image
+          ) !== "mp4" && (
+            <LazyLoadImage
+              src={
+                data.data.items[0].nft_data[0].external_data.image ||
+                `https://ivanmolto.mypinata.cloud/ipfs/QmbLmd6Zy7AU5oS9Ww6PyHZamV6QT5rF3yEWTSCvkLxLTk`
+              }
+              alt="Collection First NFT"
+              className="rounded-md"
+            />
+          )}
+      </div>
+    );
+};
+
+export default NftLogo;
